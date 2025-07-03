feee2b57206fdc22bd46caa24798b270601095f9
Lukasndok <febriyantolukas@gmail.com>
2025-07-03 23:55:35 +0700

    change url

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -4,7 +4,7 @@
 <section class="text-gray-600 body-font">
   <div class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
     <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
-      <img class="object-cover object-center rounded" alt="hero" src="{{ $featured_post->getfirstMediaurl('featured_images')}}">
+      <img class="object-cover object-center rounded" alt="hero" src="{{ $featured_post->getfirstMediaUrl('featured_images') ?? 'https://dummyimage.com/722x402'}}">
     </div>
     <div class="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
       <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">{{$featured_post->title}}</h1>
@@ -73,7 +73,7 @@
         @foreach ($post_list as $post)
         <div class="p-4 md:w-1/3 ">
         <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
-          <img class="lg:h-48 md:h-36 w-full object-cover object-center" src="{{$post->getFirstMediaurl('featured_images')  != '' ? $post->getFirstMediaurl('featured_images') : "https://dummyimage.com/722x402 "}}" alt="blog">
+          <img class="lg:h-48 md:h-36 w-full object-cover object-center" src="{{$post->getFirstMediaUrl('featured_images')  != '' ? $post->getFirstMediaurl('featured_images') : "https://dummyimage.com/722x402 "}}" alt="blog">
           <div class="p-6">
             <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">CATEGORY</h2>
             <h1 class="title-font text-lg font-medium text-gray-900 mb-3">{{ $post->title }}</h1>
