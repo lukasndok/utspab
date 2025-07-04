4726ae515b0a8f65b8da6c75979161944e2b8e3b
Lukasndok <febriyantolukas@gmail.com>
2025-07-04 22:06:54 +0700

    add produc

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,7 +22,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         if($this->app->environment('production')) {
-            $this->app['request']->server->set('HTTPS', on);
+            $this->app['request']->server->set('HTTPS', 'on');
             URL::forceScheme('https');
         }
         //
